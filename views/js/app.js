e878ce565a1a87cbd5e0111a3e8018abc2916432
Kettan <iamken1204@gmail.com>
2015-07-03 15:24:25 +0800

    add mui, reactjs, material-ui js components

--- views/js/app.js
+++ views/js/app.js
@@ -12,10 +12,4 @@
       var s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(wf, s);
     })();
-
-    var React = require('react'),
-  mui = require('material-ui'),
-  RaisedButton = mui.RaisedButton;
-
-
 </script>
